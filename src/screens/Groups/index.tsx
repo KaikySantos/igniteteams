118fd4086f89eb382b023ef501ceecf1150ecc20
kaikySantos <info.kaiky@gmail.com>
2023-01-10 14:58:21 -0300

    feat: component - list empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,9 +6,10 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 
 import { Container } from "./styles";
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocketseat'])
+  const [groups, setGroups] = useState<string[]>([])
 
   return (
     <Container>
@@ -25,6 +26,12 @@ export function Groups() {
         renderItem={({ item }) => (
           <GroupCard
             title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message="Que tal cadastrar a primeira turma?"
           />
         )}
       />
