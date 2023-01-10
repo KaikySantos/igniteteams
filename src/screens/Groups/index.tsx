50b47e18490d4c7915d7e6e0ae5a4ba235913f03
kaikySantos <info.kaiky@gmail.com>
2023-01-10 15:16:56 -0300

    feat: component - button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -7,6 +7,7 @@ import { Highlight } from "@components/Highlight";
 
 import { Container } from "./styles";
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
@@ -34,6 +35,10 @@ export function Groups() {
             message="Que tal cadastrar a primeira turma?"
           />
         )}
+      />
+
+      <Button
+        title="Criar nova turma"
       />
     </Container>
   )
