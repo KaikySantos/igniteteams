124a3c93b3448d5937cf19ce315b19bee3225801
kaikySantos <info.kaiky@gmail.com>
2023-01-10 13:33:31 -0300

    feat: external fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
 import { ThemeProvider } from 'styled-components/native';
 import theme from '@theme/index';
 
 import { Groups } from '@screens/Groups';
+import { ActivityIndicator } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
       <Groups />
     </ThemeProvider>
   );
