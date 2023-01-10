c89752873521d57bfae68be9582e4e6a5270a0f9
kaikySantos <info.kaiky@gmail.com>
2023-01-09 23:08:09 -0300

    feat: path mapping

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,7 +1,9 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export function Groups() {
   return (
-    <View></View>
+    <View>
+      <Text>Oi Oi!</Text>
+    </View>
   )
 }
