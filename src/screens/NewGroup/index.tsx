e5b1df70dc236cc94004804ec6cf3f008450450c
kaikySantos <info.kaiky@gmail.com>
2023-01-17 10:51:19 -0300

    feat: handling exceptions

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { AppError } from '@utils/AppError';
+import { Alert } from 'react-native';
 
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
@@ -17,10 +19,19 @@ export function NewGroup() {
   
   async function handleNew() {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma.')
+      }
+
       await groupCreate(group);
       navigation.navigate('players', { group });
     } catch (error) {
-      console.log(error);
+      if (error instanceof AppError) {
+        Alert.alert('Novo grupo', error.message);
+      } else {
+        Alert.alert('Novo grupo', 'Não foi possível criar um novo grupo.');
+        console.log(error);
+      }
     }
   }
 
